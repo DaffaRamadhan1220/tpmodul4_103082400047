08e863449cbe2826d751cfc62383a18a1d77f621
DaffaRamadhan1220 <daffasurya212@gmail.com>
2026-03-13 13:55:52 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103082400047
+{
+    public class KodePos
+    {
+        private Dictionary<string, int> tabelKodePos = new Dictionary<string, int>()
+    {
+        {"Batununggal", 40266},
+        {"Kujangsari", 40287},
+        {"Mengger", 40267},
+        {"Wates", 40256},
+        {"Cijaura", 40287},
+        {"Jatisari", 40286},
+        {"Margasari", 40286},
+        {"Sekejati", 40286},
+        {"Kebonwaru", 40272},
+        {"Maleer", 40274},
+        {"Samoja", 40273}
+    };
+
+        public int? getKodePos(string kelurahan)
+        {
+            if (tabelKodePos.ContainsKey(kelurahan))
+            {
+                return tabelKodePos[kelurahan];
+            }
+            else
+            {
+                Console.WriteLine("Kelurahan tidak ditemukan.");
+                return null;
+            }
+        }
+    
